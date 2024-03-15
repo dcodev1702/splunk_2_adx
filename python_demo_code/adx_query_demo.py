9a535c8e40020cd5ab5defc46fab4e366e37325e
DCODEV1702 <32214072+dcodev1702@users.noreply.github.com>
2024-03-15 10:48:06 -0400

    Update adx_query_demo.py

--- python_demo_code/adx_query_demo.py
+++ python_demo_code/adx_query_demo.py
@@ -13,28 +13,26 @@ def main():
     3. Create Database
     4. Within the Database -- Assign Admin permission to App Registration
     5. Create Table within ADX Database
-    .create table ['SplunkLog']  (['TimeGenerated']:datetime, ['Company']:string, ['Hacker']:string, ['Venue']:string, ['Type']:string)
+    .create table SplunkTable (FWLogEntry:dynamic)
 
-    6. Create Table Mapping
-    .create table SplunkLog ingestion json mapping "SplunkLog_Json_Mapping"
-    ```[{ "column" : "TimeGenerated", "datatype" : "datetime", "Properties":{"Path":"$.TimeGenerated"}},
-        { "column" : "Company", "datatype" : "string", "Properties":{"Path":"$.Company"}},
-        { "column" : "Hacker", "datatype" : "string", "Properties":{"Path":"$.Hacker"}},
-        { "column" : "Venue", "datatype" : "string", "Properties":{"Path":"$.Venue"}},
-        { "column" : "Type", "datatype" : "string", "Properties":{"Path":"$.Type"}}
-      ]```
+    // Create mapping
+    .create table SplunkTable ingestion json mapping 'SplunkTable_JSON_Mapping' '[{"column":"FWLogEntry", "Properties":{"Path":"$.FWLogEntry"}}]'
+
+    // Show mapping
+    .show table SplunkTable ingestion json mapping "SplunkTable_JSON_Mapping"
 
     6. Populate the record table with test data
     // JSON Type
-    .ingest inline into table SplunkLog with (format = "json") <| {"TimeGenerated":"2024-03-15T20:38:03.9122018Z", "Company":"TrustedSec", "Hacker":"Carlos Perez", "Venue":"BSides PR", "Type":"SplunkLog"}
+    .ingest inline into table SplunkTable with (format = "json") <| {"FWLogEntry":{"TimeGenerated":"2024-03-15T20:38:03.9122018Z", "Company":"TrustedSec", "Hacker":"Carlos Perez", "Venue":"BSides PR", "Type":"SplunkLog"}}
 
-    .ingest inline into table SplunkLog with (format = "json") <| {"TimeGenerated":"2024-03-14T19:22:43.9122018Z", "Company":"TrustedSec", "Hacker":"Edwin David", "Venue":"BSides Nova", "Type":"SplunkLog"}
+    .ingest inline into table SplunkTable with (format = "json") <| {"FWLogEntry":{"TimeGenerated":"2024-03-14T19:22:43.9122018Z", "Company":"TrustedSec", "Hacker":"Edwin David", "Venue":"BSides Nova", "Type":"SplunkLog"}}
    
-    .ingest inline into table SplunkLog with (format = "json") <| {"TimeGenerated":"2024-03-13T18:45:54.9122018Z", "Company":"TrustedSec", "Hacker":"David Kennedy", "Venue":"BSides Cleveland", "Type":"SplunkLog"}
+    .ingest inline into table SplunkTable with (format = "json") <| {"FWLogEntry":{"TimeGenerated":"2024-03-13T18:45:54.9122018Z", "Company":"TrustedSec", "Hacker":"David Kennedy", "Venue":"BSides Cleveland", "Type":"SplunkLog"}}
 
-    .ingest inline into table SplunkLog with (format = "json") <| {"TimeGenerated":"2024-03-13T18:45:54.9122018Z", "Company":"MFCC-G9-DOG", "Hacker":"JJ Bottles", "Venue":"BSides DC", "Type":"SplunkLog"}
-    
-    .ingest inline into table SplunkLog with (format = "json") <| {"TimeGenerated":"2024-03-15T12:45:54.9122018Z", "Company":"Microsoft Corp", "Hacker":"T0pCyber Cloud Forensicator", "Venue":"BSides Raleigh", "Type":"SplunkLog"}
+    .ingest inline into table SplunkTable with (format = "json") <| {"FWLogEntry":{"TimeGenerated":"2024-03-13T18:45:54.9122018Z", "Company":"MFCC-G9-DOG", "Hacker":"JJ Bottles", "Venue":"BSides DC", "Type":"SplunkLog"}}
+
+    .ingest inline into table SplunkTable with (format = "json") <| {"FWLogEntry":{"TimeGenerated":"2024-03-15T12:45:54.9122018Z", "Company":"Microsoft Corp", "Hacker":"T0pCyber Cloud Forensicator", "Venue":"BSides Raleigh", "Type":"SplunkLog"}}
+
     '''
     
     ######################################################
@@ -55,8 +53,9 @@ def main():
     
     # The authentication method will be taken from the chosen KustoConnectionStringBuilder.
     with KustoClient(kcsb) as kusto_client:
-        query = "SplunkLog \
-                | sort by TimeGenerated desc"
+        query = "SplunkTable \
+        | extend pj = parse_json(FWLogEntry) \
+        | project TimeGenerated=pj.TimeGenerated, Company=pj.Company, Hacker=pj.Hacker, Venue=pj.Venue, Type=pj.Type"
         
         response = kusto_client.execute(KUSTO_DATABASE, query)
       
@@ -68,7 +67,6 @@ def main():
             idx += 1
           
         kusto_client.close()
-
 
 if __name__ == "__main__":
     main()
